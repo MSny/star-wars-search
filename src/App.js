9b42d9323db233658defc60226e4ab6e27f01a26
Meir Snyder <msny36@gmail.com>
2018-10-29 17:19:14 -0400

    added fonts, material-ui, basic workflow

--- src/App.js
+++ src/App.js
@@ -1,26 +1,108 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import axios from 'axios';
+import {
+  Typography,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Grid,
+  Input,
+  Button,
+} from '@material-ui/core';
 
-class App extends Component {
+// import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    maxWidth: 600,
+    padding: theme.spacing.unit * 2,
+  },
+  starText:{
+    fontFamily: 'STARWARS'
+  }
+});
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { 
+      characters: [],
+      error: null,
+      selectedCharacter: '',
+      movieInfo: {} 
+    };
+  }
+
+  async componentDidMount() {
+   
+      const url = `data/character.json`;
+      axios.get(url).then(response => this.setState({characters: response.data.characters}) );
+  
+    // const response = await fetch(`../data/character.json`);
+    // const json = await response.json();
+    // this.setState({ data: json });
+  }
+
+  
+
+
+  handleChange = event => {
+    this.getCharacterMovies(event.target.value);
+    this.setState({ [event.target.name]: event.target.value });
+    console.log(this.state.selectedCharacter);
+  };
+  
+  getCharacterMovies = (url) =>{
+    axios.get(url).then(response => this.setState({movieInfo: response.data}) );
+    console.log(this.state.movieInfo);
+  }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+    console.log(this.state.characters);
+    let data = this.state.characters;
+    console.log("data ", data)
+    let characterList;
+    if (data.length > 1){
+      characterList = (
+        <Select
+            value={this.state.selectedCharacter}
+            onChange={this.handleChange}
+            input={<Input name="selectedCharacter" id="character-helper" />}
           >
-            Learn React
-          </a>
-        </header>
-      </div>
+          <MenuItem value="">
+              <em>Choose Character</em>
+            </MenuItem>
+            
+          {data.map(character => (
+             <MenuItem value={character.url}>{character.name}</MenuItem>
+          ))}
+         </Select>
+      )
+    }
+    else {
+      characterList = (
+        <select></select>
+      )
+    }
+
+    return (
+      <Grid container
+      direction="column"
+      justify="center"
+      alignItems="center">
+      <Typography variant='h2' style={{fontFamily: "STARWARS"}}>STAR WARS SEARCH</Typography>
+      <Grid item>
+         <form autoComplete="off">
+         <FormControl>
+          <InputLabel htmlFor="character-helper">Character</InputLabel>
+                  {characterList}
+
+            </FormControl>
+         </form>
+         </Grid>
+      </Grid>
     );
   }
 }
